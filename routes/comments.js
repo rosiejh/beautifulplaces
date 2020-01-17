1e1fd3fcb01d137f4bca07d751871eab269a48ee
Junghwa Yang <rosie.junghwa@gmail.com>
2020-01-17 14:49:51 +1100

    [Add] Edit and update comment

--- routes/comments.js
+++ routes/comments.js
@@ -42,6 +42,28 @@ router.post('/', isLoggedIn, function (req, res) {
     });
 });
 
+// Comment EDIT route
+router.get('/:comment_id/edit', function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render('comments/edit', {place_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+// Comment UPDATE route
+router.put('/:comment_id', function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect('/place/' + req.params.id);
+        }
+    });
+});
+
 // middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
