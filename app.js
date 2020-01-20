f230cc543c8633ae3504174cd0b1d8bf91ef5cc0
Junghwa Yang <rosie.junghwa@gmail.com>
2020-01-20 18:42:04 +1100

    change mongodb

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes = require('./routes/comments'),
     placeRoutes   = require('./routes/place'),
     indexRoutes   = require('./routes/index');
 
-mongoose.connect('mongodb://localhost/bepl', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://rosie:rosiebepl@rosie-kkjzg.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
